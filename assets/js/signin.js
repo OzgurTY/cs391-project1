7b314a5eb5d1af7b5f17c4c9e7818e3d2e3b7191
OzgurTY <ozgur.yavuz@ozu.edu.tr>
2024-04-04 23:28:29 +0300

    first commit

---
+++ assets/js/signin.js
@@ -0,0 +1,55 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+
+/* -------------------------SIGN UP CHECKER---------------------------------------------------*/ 
+
+let existingUsers = [
+    
+];
+
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+
+    
+    let userExists = existingUsers.some(user => user.username === username);
+    let emailExists = existingUsers.some(user => user.email === email);
+
+    
+    if (userExists) {
+        showError("Username already exists");
+    } else if (emailExists) {
+        showError("Email already exists");
+    } else {
+        
+        existingUsers.push({ username: username, email: email });
+        
+        let password = document.getElementById("password").value;
+
+        
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+
+        
+        console.log("Signup successful");
+    }
+});
+
+function showError(message) {
+    let errorMessageElement = document.getElementById("error-message");
+    errorMessageElement.textContent = message;
+}
+/*--------------------------------------------------------------------------------------------*/ 

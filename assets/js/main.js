e97545f9aa312cd75dd0881fc7a371587b75fc54
berkerkarakus <76535553+berkerkarakus@users.noreply.github.com>
2024-04-07 02:14:02 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,23 +21,31 @@ const searchBarIsActive = () => navbarForm.classList.toggle('active');
 navbarSearchBtn.addEventListener('click', searchBarIsActive);
 navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
 
- document.getElementById("genre-select").addEventListener('change', function() {
-        var selectedGenre = this.value.toLowerCase(); // Convert selected genre to lowercase for comparison
 
-        const movies = document.querySelectorAll('.movie-card'); // Select all movie cards
+function filterMovies() { //filters functions based on the option value of filter bar
+    const selectedGenre = document.getElementById("genre-select").value.toLowerCase();
+    const selectedYear = document.getElementById("year-select").value.toLowerCase();
+    const movies = document.querySelectorAll('.movie-card');
+
+    movies.forEach(movie => {
+        const genreText = movie.querySelector('.genre').textContent.toLowerCase();
+        const yearText = movie.querySelector('.year').textContent;
+        const year = parseInt(yearText, 10);
+
+        const matchesGenre = selectedGenre === 'all genres' || genreText.includes(selectedGenre);
+
+       
+        let matchesYear = selectedYear === 'all years';
+        if (selectedYear === '2024' && year === 2024) matchesYear = true;
+        else if (selectedYear === '2020-2023' && year >= 2020 && year <= 2023) matchesYear = true;
+        else if (selectedYear === '2010-2019' && year >= 2010 && year <= 2019) matchesYear = true;
+        else if (selectedYear === '2000-2009' && year >= 2000 && year <= 2009) matchesYear = true;
+        else if (selectedYear === '1970-1999' && year >= 1970 && year <= 1999) matchesYear = true;
+
         
-        movies.forEach(movie => {
-            // Extract genre text from each movie card and convert it to lowercase
-            const genreText = movie.querySelector('.genre').textContent.toLowerCase();
-            
-            // Decision to show/hide based on genre match or 'all genres' selection
-            if (selectedGenre === 'all genres') {
-                movie.style.display = ''; // Show the movie card
-            } else if (genreText.includes(selectedGenre)) {
-                movie.style.display = ''; // Show if it matches the selected genre
-            } else {
-                movie.style.display = 'none'; // Hide if it does not match
-            }
-        });
+        movie.style.display = matchesGenre && matchesYear ? '' : 'none';
     });
-
+}
+document.getElementById("genre-select").addEventListener('change', filterMovies);
+document.getElementById("year-select").addEventListener('change', filterMovies);
+    

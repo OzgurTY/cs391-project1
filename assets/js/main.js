723e14121aea397017854662088aefd4b7c1001b
berkerkarakus <76535553+berkerkarakus@users.noreply.github.com>
2024-04-07 01:33:20 +0300

    Update main.js
    deneme

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,24 @@ const searchBarIsActive = () => navbarForm.classList.toggle('active');
 
 navbarSearchBtn.addEventListener('click', searchBarIsActive);
 navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+
+ document.getElementById("genre-select").addEventListener('change', function() {
+        var selectedGenre = this.value.toLowerCase(); // Convert selected genre to lowercase for comparison
+
+        const movies = document.querySelectorAll('.movie-card'); // Select all movie cards
+        
+        movies.forEach(movie => {
+            // Extract genre text from each movie card and convert it to lowercase
+            const genreText = movie.querySelector('.genre').textContent.toLowerCase();
+            
+            // Decision to show/hide based on genre match or 'all genres' selection
+            if (selectedGenre === 'all genres') {
+                movie.style.display = ''; // Show the movie card
+            } else if (genreText.includes(selectedGenre)) {
+                movie.style.display = ''; // Show if it matches the selected genre
+            } else {
+                movie.style.display = 'none'; // Hide if it does not match
+            }
+        });
+    });
+

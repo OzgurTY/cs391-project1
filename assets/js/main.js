e9b237defa0e7b552deb67306ff77eb60df46877
Ozgur Tuna Yavuz <104658888+OzgurTY@users.noreply.github.com>
2024-04-07 16:05:53 +0000

    version1.12847192

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,29 +156,4 @@ document.addEventListener('DOMContentLoaded', function() {
     updateButton(); 
 });
 
-/*
-const container = document.getElementById('movies-grid');
-const items = document.querySelectorAll('[category="movie"]');
 
-items.forEach(item => {
-    container.appendChild(item.cloneNode(true));
-});
-*/
-
-
-function cloneAndAppendElements() {
-    // Get the container with the elements to clone from index.html
-    const contentToClone = document.getElementById('indexmovie');
-
-    // Get the container to append the cloned elements to in movies.html
-    const moviesContainer = document.getElementById('movies-grid');
-
-    // Clone each specific HTML element and append it to the movies container
-    contentToClone.querySelectorAll('.movie-card').forEach(movie => {
-        const clonedMovie = movie.cloneNode(true);
-        moviesContainer.appendChild(clonedMovie);
-    });
-}
-
-// Call the function to clone and append elements when the page loads
-window.onload = cloneAndAppendElements;
